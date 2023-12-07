45834d21bce96b22dfd192f149fefb06fe9d6a27
Maria Messaoud-Nacer <maria.messaoudnacer@mfa.fr>
2023-12-07 18:33:08 +0100

    ✅ test

--- main.c
+++ main.c
@@ -5,23 +5,29 @@
 
 int main()
 {
+    int i = 2;
     // Initialisation de la graine pour rand()
     srand(time(0));
     int depth = 3;
     GlobalGrid game = initialize_global_grid();
 
-    while (1)
-    {
+    // while (1)
+    // {
 
-        if (game_CheckIfWon(&game) || isGlobalGridFull(game))
-        {
-            Display_game(&game);
-            break;
-        }
-        else
-        {
-            UTTT_GAME(&game, random_pick_move, alphaBeta_pick_move, depth);
-        }
+    //     if (game_CheckIfWon(&game) || isGlobalGridFull(game))
+    //     {
+    //         Display_game(&game);
+    //         break;
+    //     }
+    //     else
+    //     {
+    //         UTTT_GAME(&game, random_pick_move, alphaBeta_pick_move, depth);
+    //     }
+    // }
+    while (i > 0)
+    {
+        UTTT_GAME(&game, random_pick_move, minimax_pick_move, depth);
+        i--;
     }
 
     return 0;
